912f930cd3675ac95fd07a36e73f1910d1a422bc
ookann <36696085+ookann@users.noreply.github.com>
2019-05-03 11:00:58 +0300

    application

---
+++ Proje Kodları/Restoran/Class/Islemler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    class Islemler
+    {
+        public static string dondur(string metin)
+        {
+            return metin;
+        }
+        
+    }
+}
---
+++ Proje Kodları/Restoran/Class/Veritabani.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Kafe_Restorant_Yonetim_Sistemi
+{
+    class Veritabani
+    {
+
+        public static OleDbConnection con = new OleDbConnection("Provider=Microsoft.JET.OLEDB.4.0; Data Source = Veritabani/restoran.mdb");//baglan 
+      
+        public static void baglantiKontrol()//Bağlantı kapalıysa açmak için kullandık
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    con.Open();
+                }
+                catch
+                {
+                    MessageBox.Show("Veri Tabanı Bağlantısı Yapılamadı");
+                    Application.Exit();
+                }
+            }
+        }
+
+        public static DataTable VeriGetir(string sql) //işlemler için
+        {
+            baglantiKontrol();
+            DataTable dt = new DataTable();
+            OleDbCommand cmd = new OleDbCommand(sql, con);
+            OleDbDataReader dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
